a674ecb19842137338590d836fbf39900f28a7b2
harshuh <harshh1124@gmail.com>
2026-07-05 04:39:37 +0530

    Add project files.

---
+++ Models/Employee/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace EMS.Models.Employee
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+
+        public required string Role { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required int Phone {get; set;}
+        public decimal? Salary { get; set; }
+
+    }
+}
