69963b9ae83aa57336cb4ce02a2e52b0c3548a38
harshuh <harshh1124@gmail.com>
2026-07-05 13:52:25 +0530

    added get and post routes

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿using Azure.Core;
+using EMS.Models.Employee;
+using EMS.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly AppDbContext appdbcontext;
+
+        public EmployeeController(AppDbContext appdbcontext)
+        {
+            this.appdbcontext = appdbcontext;
+        }
+
+        // Get All the Employees
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var data = appdbcontext.Employees.ToList();
+
+            return Ok(data);
+        }
+
+        // Add or insert the Employees
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            var body = new Employee()
+            {
+                EmployeeId = employee.EmployeeId,
+                Role = employee.Role,
+                Name = employee.Name,
+                Email = employee.Email,
+                Phone = employee.Phone,
+                Salary = employee.Salary
+            };
+
+            appdbcontext.Employees.Add(body);
+            appdbcontext.SaveChanges();
+
+            return Ok(body);
+
+
+        }
+    }
+}
